79e81c2ce9f0eb37021445972957a348c3d7255d
LucaBertuolo <berto990@hotmail.it>
2015-01-25 13:21:21 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='AriettaSHT7x',
+      version='1.0',
+      description='Python module to use Sensirion SHT7x sensor on Acme Arietta G25',
+      author='Luca Bertuolo',
+      author_email='berto990@hotmail.it',
+      url='https://github.com/LucaBertuolo/AriettaSHT7x',
+      py_modules=['AriettaSHT7x']
+     )
